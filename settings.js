ab1be00cf3f2875ec88a7f8ec992e6e51d006616
nusatyiz <130839059+nusatyiz@users.noreply.github.com>
2023-04-24 20:08:11 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,14 +11,14 @@ global.APIKeys = {
 }
 
 global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
-global.packname = namabot
-global.author = namaowner
+global.namaowner = "nusatyiz"
+global.packname = "nusabotyiz"
+global.author = "nusatyiz"
 global.myweb = "https://s.id/zeeoneofc"
-global.footer_text = "© " + namabot
+global.footer_text = "©nusatyiz " + "nusatyiz"
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
 global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
+global.owner = ['6282192834347']
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
 global.prefa = ['-_-']
