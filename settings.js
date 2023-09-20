f68387664a87f633d880ae026d824cc76475771a
ditt12 <145573395+ditt12@users.noreply.github.com>
2023-09-20 21:00:23 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,15 +10,15 @@ global.APIKeys = {
 	'https://api.zeeoneofc.my.id': 'Your Key',
 }
 
-global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
-global.packname = namabot
-global.author = namaowner
+global.namabot = "belutgaraga"
+global.namaowner = "dittofc"
+global.packname = belutgaraga
+global.author = dittofc
 global.myweb = "https://s.id/zeeoneofc"
-global.footer_text = "© " + namabot
+global.footer_text = "© " + belutgaraga
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
 global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
+global.owner = ['6282141069025']
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
 global.prefa = ['-_-']
