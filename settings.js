d4ddc1e0a5512f76aa08d52b068b90f3012fb66b
Kyzimkece1 <150445658+Kyzimkece1@users.noreply.github.com>
2023-11-12 20:54:51 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,24 +7,25 @@ global.APIs = {
 
 // Apikey
 global.APIKeys = {
-	'https://api.zeeoneofc.my.id': 'Your Key',
+	'https://api.zeeoneofc.my.id': 'HvoASbQvjQYxXhp',
 }
 
-global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
-global.packname = namabot
-global.author = namaowner
+global.namabot = "Bot Kyzim"
+global.namaowner = "Fadly"
+global.packname = Kyzim Suka Kamu
+global.author = Kyzim anak baik
 global.myweb = "https://s.id/zeeoneofc"
 global.footer_text = "© " + namabot
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
 global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
+global.owner = ['6282235335847']
 global.pairingNumber = "" //pairing number to install bot 
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
 global.prefa = ['-_-']
 global.grup_only = false //bot hanya bisa di gunakan dalam grup jika true
 global.mess = {
+global.prefa = ['😱','😁','👀','🐦','🐤','🗿']
     done: 'Done ✅',
     wait: '_⌛ Sedang di proses_',
     admin: '_Fitur Khusus Admin Group_',
