75f547c43502cbf6ba843dcfb00cca4c126a5a36
XinotBot <99858090+XinotBot@users.noreply.github.com>
2023-12-19 08:29:21 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,18 +7,18 @@ global.APIs = {
 
 // Apikey
 global.APIKeys = {
-	'https://api.zeeoneofc.my.id': 'Your Key',
+	'https://api.zeeoneofc.my.id': 'wmmLEYjaUObZYmY',
 }
 
-global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
-global.packname = namabot
-global.author = namaowner
+global.namabot = "Shadow Bot"
+global.namaowner = "Cid Kagenou"
+global.packname = Shadow Bot
+global.author = Kagenou
 global.myweb = "https://s.id/zeeoneofc"
-global.footer_text = "© " + namabot
+global.footer_text = "© " + Shadow Bot
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
 global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
+global.owner = ['6281312443975']
 global.pairingNumber = "" //pairing number to install bot 
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
